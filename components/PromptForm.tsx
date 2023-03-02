efdfd9b145a9dcb1c7f0b336de7b14395eb11b46
Philani Ncube <38924116+PhilaniNcube@users.noreply.github.com>
2023-03-02 15:56:07 +0200

    question

--- components/PromptForm.tsx
+++ components/PromptForm.tsx
@@ -36,13 +36,13 @@ const PromptForm = () => {
 
   return (
     <div className="max-w-7xl mx-auto py-10">
-      <form onSubmit={handleSubmit} className="w-1/2 mx-auto py-10">
+      <form onSubmit={handleSubmit} className="w-1/2 py-10">
         <div className="flex flex-col md:mr-16">
           <label
             htmlFor="prompt"
             className="text-gray-800 dark:text-gray-100 text-sm font-bold leading-tight tracking-normal mb-2"
           >
-            Generate Product Name
+           What is your question?
           </label>
           <input
             id="prompt"
