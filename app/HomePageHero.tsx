49cedcf583a9f9175a51ab10c29c0a66a186691e
Philani Ncube <38924116+PhilaniNcube@users.noreply.github.com>
2023-03-02 15:57:24 +0200

    cheange hero text

--- app/HomePageHero.tsx
+++ app/HomePageHero.tsx
@@ -4,7 +4,7 @@ const HomePageHero = () => {
   return <header className="relative isolate">
     <Image src="/images/ai_intelligence.jpg" width={1920} height={1280} alt="Artificial Intelligence" className="aspect-[3/1] w-full object-cover" />
     <div className="absolute inset-0 bg-slate-200/30 flex justify-center items-center">
-      <h1 className="text-center text-2xl font-bold text-black">Lets Generate Text & Images with Open-AI</h1>
+      <h1 className="text-center text-2xl font-bold text-black">Ask Me Your Question</h1>
     </div>
   </header>;
 };
