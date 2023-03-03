be2b2db2017b9da6f7c144d64188f870f03018fc
Philani Ncube <ncbphi001@gmail.com>
2023-03-03 16:10:35 +0200

    streaming prompt responses

--- components/PromptForm.tsx
+++ components/PromptForm.tsx
@@ -6,52 +6,73 @@ const PromptForm = () => {
 
   const [loading, setLoading] = useState(false)
   const [prompt, setPrompt] = useState('')
+  const [question, setQuestion] = useState('')
   const [promptResponse, setPromptResponse] = useState("");
 
   const handleSubmit = async(e:FormEvent<HTMLFormElement>) => {
     e.preventDefault()
-
+    setPromptResponse('')
     setLoading(true)
 
-    const response = await fetch(`/api/generate`, {
-      method: 'POST',
+
+
+        const {
+       prompt
+        } = Object.fromEntries(new FormData(e.currentTarget));
+
+        console.log({prompt})
+
+    const response = await fetch("/api/generate", {
+      method: "POST",
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        prompt
-      })
+        prompt: prompt,
+      }),
     });
 
     if(!response.ok) {
+      setLoading(false);
       throw new Error(response.statusText);
-
     }
 
-    let answer = await response.json();
-    console.log(answer);
-    setPromptResponse(answer.choices[0].text);
+    const data = response.body;
+    if (!data) {
+      return;
+    }
+  const reader = data.getReader();
+  const decoder = new TextDecoder();
+  let done = false;
+
+  while (!done) {
+    const { value, done: doneReading } = await reader.read();
+    done = doneReading;
+    const chunkValue = decoder.decode(value);
     setLoading(false)
+
+    setPromptResponse((prev) => prev + chunkValue);
+  }
+
+  setLoading(false);
+
   }
 
   return (
     <div className="max-w-7xl mx-auto py-10">
-      <form onSubmit={handleSubmit} className="w-1/2 py-10">
+      <form onSubmit={handleSubmit} className="w-3/4 py-10">
         <div className="flex flex-col md:mr-16">
           <label
             htmlFor="prompt"
             className="text-gray-800 dark:text-gray-100 text-sm font-bold leading-tight tracking-normal mb-2"
           >
-           What is your question?
+            What is your question?
           </label>
           <input
+            type="text"
             id="prompt"
             name="prompt"
-            type="text"
-            value={prompt}
-            onChange={(e) => setPrompt(e.target.value)}
             className="text-gray-600 dark:text-gray-400 focus:outline-none focus:border focus:border-indigo-700 dark:focus:border-indigo-700 dark:border-gray-700 dark:bg-gray-800 bg-white font-normal w-64 h-10 flex items-center pl-3 text-sm border-gray-300 rounded border shadow"
-            placeholder="Placeholder"
           />
         </div>
 
@@ -63,7 +84,7 @@ const PromptForm = () => {
         </button>
       </form>
 
-      <h2 className="text-lg font-bold text-slate-400 mt-6">
+      <h2 className="text-lg font-medium text-slate-400 mt-6">
         Response: {loading ? "Loading..." : promptResponse}
       </h2>
     </div>
--- pages/api/generate.ts
+++ pages/api/generate.ts
@@ -1,51 +1,61 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { OpenAIStream, OpenAIStreamPayload } from "../../utils/OpenAIStream";
 
 if(!process.env.OPENAI_API_KEY) {
   throw new Error('Missing env var from OpenAI')
 }
 
+export const config = {
+  runtime: "edge",
+};
+
+
+
 type Data = {
   name: string
 }
 
+ type Payload = {
+     model: string,
+    prompt:string,
+    temperature: number,
+    top_p: number,
+    frequency_penalty: number,
+    presence_penalty: number,
+    max_tokens: number,
+    stream: boolean,
+    n: number,
+}
+
+
+
 export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+  req: Request
+) :Promise<Response>{
 
+  const { prompt } = (await req.json()) as {
+    prompt?: string;
+  };
 
-  const {prompt} = req.body
 
   if(!prompt) {
     return new Response("No prompt in the request", {status:400})
   }
 
-  const payload = {
-    model: 'text-davinci-003',
-    prompt,
+  const payload :OpenAIStreamPayload= {
+    model: 'gpt-3.5-turbo',
+    messages:[{role:"user", content:prompt}],
     temperature: 0.7,
     top_p: 1,
     frequency_penalty: 0,
     presence_penalty: 0,
-    max_tokens: 90,
-    stream: false,
+    max_tokens: 200,
+    stream: true,
     n: 1,
   }
 
+ const stream = await OpenAIStream(payload);
+  return new Response(stream);
 
-  const response = await fetch("https://api.openai.com/v1/completions", {
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${process.env.OPENAI_API_KEY ?? ""}`,
-    },
-    method: "POST",
-    body: JSON.stringify(payload)
-  })
-
-  const json = await response.json()
-
-
-
-  res.status(200).json(json)
 }
